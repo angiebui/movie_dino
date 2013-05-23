cb385373b8ce9232bce23cd344c27141df2e509d
vincentstorme <storme.vincent@gmail.com>
2013-05-23 16:52:44 -0700

    added attr_accessible :cache_date

--- app/models/theater.rb
+++ app/models/theater.rb
@@ -1,5 +1,5 @@
 class Theater < ActiveRecord::Base
-  attr_accessible :name, :address, :phone_number
+  attr_accessible :name, :address, :phone_number, :cache_date
   has_many :showtimes
   has_many :movies, :through => :showtimes
 end
