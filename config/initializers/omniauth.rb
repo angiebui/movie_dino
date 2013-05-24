031344567f653de3d612bc7af660a16a096234b9
Sean Browning <seanbrowning@gmail.com>
2013-05-23 21:29:26 -0700

    environment?

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 
 Rails.application.config.middleware.use OmniAuth::Builder do 
-  provider :facebook, FACEBOOK_APP_ID, FACEBOOK_SECRET
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
 end
