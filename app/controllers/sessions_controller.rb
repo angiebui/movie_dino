1e62445bf9bd87ded03b6a74bc523d3369a2a9a1
angiebui <angiebui@gmail.com>
2013-05-23 22:39:35 -0700

    added skeleton for outings, able to pull movies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
-  
+
   def index
   end
 
   def create
     user = User.from_omniauth(env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to root_path
+    redirect_to '/users'
   end
 
   def destroy
