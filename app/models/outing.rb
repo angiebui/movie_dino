5a6ab5be18f1257974f6a648af8037e501f80ecc
Sean Browning <seanbrowning@gmail.com>
2013-05-25 09:55:45 -0700

    added hash link related callbacks and validations to outings

--- app/models/outing.rb
+++ app/models/outing.rb
@@ -1,8 +1,17 @@
 class Outing < ActiveRecord::Base
   attr_accessible :user, :user_id, :final_selection_id
 
-  has_many :selections, :as => :owner
+  has_many :selections, as: :owner
   has_many :attendees
   belongs_to :user
 
+  before_validation :generate_link, on: :create
+
+  validates_uniqueness_of :link
+
+
+  private
+  def generate_link
+    self.link = '/' + SecureRandom.hex(3)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130525001950) do
+ActiveRecord::Schema.define(:version => 20130525163627) do
 
   create_table "attendees", :force => true do |t|
     t.integer  "outing_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20130525001950) do
     t.integer  "final_selection_id"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
+    t.string   "link"
   end
 
   create_table "selections", :force => true do |t|
