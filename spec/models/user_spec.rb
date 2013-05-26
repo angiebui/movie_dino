5aca74f0df6d23b8a509b15eb2fea261bd9c191e
Sean Browning <seanbrowning@gmail.com>
2013-05-25 20:28:51 -0700

    Unit tests for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:outings) }
+
+  describe 'attributes' do
+    let(:user) { create(:user) }
+    it 'should include email' do
+      user.email.should eq 'email@example.com'
+    end
+    it 'should include first_name' do
+      user.first_name.should eq 'Robby'
+    end
+    it 'should include last_name' do
+      user.last_name.should eq 'Bobby'
+    end
+    it 'should include an image' do
+      user.image.should eq 'http://www.example.com/1'
+    end
+  end
 end
