6d623ecdc78cdb5d3331966a7f91bad16bd5cb4d
Sean Browning <seanbrowning@gmail.com>
2013-05-26 22:19:12 -0700

    theater partial for displaying nested movie and times

--- app/models/selection.rb
+++ app/models/selection.rb
@@ -1,5 +1,6 @@
 class Selection < ActiveRecord::Base
-  attr_accessible :outing_id, :attendee_id, :showtime, :owner, :movie, :theater
+  attr_accessible :outing_id, :attendee_id, :showtime, :owner, :movie, :theater,
+    :time
 
   belongs_to :owner, :polymorphic => true
   belongs_to :showtime
