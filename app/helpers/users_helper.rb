f522af46660018b7e9d558589284fc6a5c18b083
vincentstorme <storme.vincent@gmail.com>
2013-05-28 11:34:51 -0700

    can get all data for create outings

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,21 @@
 module UsersHelper
 
   def created_outings_data
-    outings = []
-    count = 0
+    outings = {}
     current_user.outings.each do |outing|
-      outings << {:"outing_#{count}" => {:selections => outing.top_selections}}
-      count += 1
+      outings.merge!(outing.id => outing.top_selections)
     end
     outings
+  end
+
+  def style_outings_data
+    @outings.each_key do |outing|
+      puts 'OUTING BELOW$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+      puts Outing.where(id: outing)
+      @outings[outing].each do |selection|
+        puts selection
+      end
+    end
   end
 
 end
