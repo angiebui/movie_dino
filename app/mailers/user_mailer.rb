092e5891025858d2678a478e4b7c6c9fa4445602
vincentstorme <storme.vincent@gmail.com>
2013-05-28 13:55:04 -0700

    send to user name instead of email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,13 +4,17 @@ class UserMailer < ActionMailer::Base
   def outing_invite(user_id, outing_id)
     @user = User.find(user_id)
     @outing = Outing.find(outing_id)
-    mail(to: @user.email, subject: "Share your Movie Dino event with friends!")
+    email_with_name = "#{@user.first_name} <#{@user.email}>"
+
+    mail(to: email_with_name, subject: "Share your Movie Dino event with friends!")
   end
 
   def outing_result(user_id, outing_id)
     @user = User.find(user_id)
     @outing = Outing.find(outing_id)
-    mail(to: @user.email, subject: "Results are in. Time to watch a movie!")
+    email_with_name = "#{@user.first_name} <#{@user.email}>"
+    
+    mail(to: email_with_name, subject: "Results are in. Time to watch a movie!")
   end
 end
 
