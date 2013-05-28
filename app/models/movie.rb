4d71fb9173566ab83d1d5e4325e06054c12de32a
Sean Browning <seanbrowning@gmail.com>
2013-05-28 14:47:24 -0700

    optimize s3 image resizing and uploading

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -49,10 +49,10 @@ class Movie < ActiveRecord::Base
 
   def store_image(img_url)
     bucket = AWS::S3.new.buckets['moviedino']
-    image = MiniMagick::Image.open(img_url)
-    image.resize('400')
     obj = bucket.objects[self.filename]
     unless obj.exists?
+      image = MiniMagick::Image.open(img_url)
+      image.resize('400')
       obj.write(acl: :public_read, single_requiest: true, content_type: 'image/gif', data: image.to_blob)
     end
     self.update_attributes(poster: obj.public_url.to_s)
