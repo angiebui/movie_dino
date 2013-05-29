d0dd4e3a1142e546193d0a7cbdad517de52c45d0
vincentstorme <storme.vincent@gmail.com>
2013-05-29 12:01:09 -0700

    more than one chart!

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -7,30 +7,51 @@ function tabbing() {
   });
 };
 
-
-
-$(document).ready(function() {
-  tabbing();
-
+function chart() {
   require([
     "dojox/charting/Chart",
     "dojox/charting/plot2d/Pie",
     "dojox/charting/action2d/Tooltip",
     "dojox/charting/action2d/MoveSlice",
-    "dojox/charting/themes/PlotKit/green",
+    "dojox/charting/themes/PlotKit/cyan",
     "dojo/domReady!"
-], function(Chart, Pie, Tooltip, MoveSlice, PlotKitGreen){
- 
-    var pieChart = new Chart("pieChartNode");
-    pieChart.setTheme(PlotKitGreen);
-    pieChart.addPlot("default", {
-        type: "Pie",
-        fontColor: "#000"
-    });
-    pieChart.addSeries("Series A", [5,8,10,40,20]);
-    new MoveSlice(pieChart,"default");
-    new Tooltip(pieChart,"default");
-    pieChart.render();
-});
+  ], function(Chart, Pie, Tooltip, MoveSlice, PlotKitCyan){
+
+      var pieChart = new Chart("pieChartNode0");
+      pieChart.setTheme(PlotKitCyan);
+      pieChart.addPlot("default", {
+          type: "Pie",
+          fontColor: "#000"
+      });
+      pieChart.addSeries('Movie Chart', [4,4,1]);
+      new MoveSlice(pieChart,"default");
+      new Tooltip(pieChart,"default");
+      pieChart.render();
+  });
+    require([
+    "dojox/charting/Chart",
+    "dojox/charting/plot2d/Pie",
+    "dojox/charting/action2d/Tooltip",
+    "dojox/charting/action2d/MoveSlice",
+    "dojox/charting/themes/PlotKit/cyan",
+    "dojo/domReady!"
+  ], function(Chart, Pie, Tooltip, MoveSlice, PlotKitCyan){
+
+      var pieChart = new Chart("pieChartNode1");
+      pieChart.setTheme(PlotKitCyan);
+      pieChart.addPlot("default", {
+          type: "Pie",
+          fontColor: "#000"
+      });
+      pieChart.addSeries('Movie Chart', [2,2,1,23,5,3,6]);
+      new MoveSlice(pieChart,"default");
+      new Tooltip(pieChart,"default");
+      pieChart.render();
+  });
+};
+
+$(document).ready(function() {
+  tabbing();
+  chart();
 });
 
