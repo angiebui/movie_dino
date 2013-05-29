6a4f0b6185f20f1d39659200b0e29b569456a88b
vincentstorme <storme.vincent@gmail.com>
2013-05-29 10:29:27 -0700

    tabs doneish, working on graphs

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,5 +1,7 @@
 function tabbing() {
   $('.outing-box').on('click', '.profile-button', function(){
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
     $(this).siblings('.box').hide();
     $(this).siblings('.' + ($(this).attr('id')) + '-box').show();
   });
