334d98b9847aac06786a188c0452ad740efebc3b
Ed Shadi <edshadi@gmail.com>
2013-05-29 11:53:52 -0700

    move rotten_api related methods to rotten_api library, which needs some love and refactoring.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,5 @@
-
 class Movie < ActiveRecord::Base
-  ROTTEN_ADDRESS = 'http://api.rottentomatoes.com/api/public/v1.0'
-  ROTTEN_API = ENV['ROTTEN_APP_ID']
+  include RottenApi
 
   after_commit :sync_after_create, :on => :create
 
@@ -17,37 +15,8 @@ class Movie < ActiveRecord::Base
     self.title.titleize
   end
 
-  def fetch_movie_count(count_request)
-    movies_json = uri_to_json(count_request)
-    movies_json['total']
-  end
-
   def sync_after_create
     MovieWorker.perform_async(self.id)
-  end
-
-  def sync_with_rotten_api
-    movie_address = ROTTEN_ADDRESS + '/movies.json?apikey=' + ROTTEN_API + "&q=#{self.title.gsub('[^a-zA-Z\d\s&]', '').gsub(' an imax 3d experience', '').gsub(' ', '%20')}"
-    search_results = uri_to_json(movie_address)
-
-    good_results = search_results['movies'].select {|movie| movie['release_dates']['theater'] != nil}
-    good_results.reject! {|movie| Time.parse(movie['release_dates']['theater']).year > Time.now.year}
-
-    good_results.sort_by! do |movie|
-      Time.parse(movie['release_dates']['theater'])
-    end
-
-    matched_result = good_results.last
-
-    raise "Empty results from RT API" unless matched_result
-    self.update_attributes(:poster_large   => matched_result['posters']['original'],
-                           :synopsis       => matched_result['synopsis'],
-                           :poster_med     => matched_result['posters']['profile'],
-                           :runtime        => matched_result['runtime'],
-                           :mpaa_rating    => matched_result['mpaa_rating'],
-                           :critics_score  => matched_result['ratings']['critics_score'],
-                           :audience_score => matched_result['ratings']['audience_score'])
-    store_image(self.poster_large)
   end
 
   def store_image(img_url)
@@ -61,16 +30,7 @@ class Movie < ActiveRecord::Base
     self.update_attributes(poster: obj.public_url.to_s)
   end
 
-
-
   def filename
     self.title.gsub(/ /,'-')
   end
-
-  def uri_to_json(url)
-    uri = URI(url)
-    request = Net::HTTP::get(uri)
-    movies_json = JSON.parse(request)
-  end
-  
 end
---
+++ lib/rotten_api.rb
@@ -0,0 +1,38 @@
+module RottenApi
+  ROTTEN_ADDRESS = 'http://api.rottentomatoes.com/api/public/v1.0'
+  ROTTEN_API = ENV['ROTTEN_APP_ID']
+
+  def sync_with_rotten_api
+    movie_address = ROTTEN_ADDRESS + '/movies.json?apikey=' + ROTTEN_API + "&q=#{self.title.gsub('[^a-zA-Z\d\s&]', '').gsub(' an imax 3d experience', '').gsub(' ', '%20')}"
+    search_results = uri_to_json(movie_address)
+
+    good_results = search_results['movies'].select {|movie| movie['release_dates']['theater'] != nil}
+    good_results.reject! {|movie| Time.parse(movie['release_dates']['theater']).year > Time.now.year}
+
+    good_results.sort_by! do |movie|
+      Time.parse(movie['release_dates']['theater'])
+    end
+
+    matched_result = good_results.last
+
+    raise "Empty results from RT API" unless matched_result
+    self.update_attributes(:poster_large   => matched_result['posters']['original'],
+                           :synopsis       => matched_result['synopsis'],
+                           :poster_med     => matched_result['posters']['profile'],
+                           :runtime        => matched_result['runtime'],
+                           :mpaa_rating    => matched_result['mpaa_rating'],
+                           :critics_score  => matched_result['ratings']['critics_score'],
+                           :audience_score => matched_result['ratings']['audience_score'])
+    store_image(self.poster_large)
+  end
+
+  def fetch_movie_count(count_request)
+    uri_to_json(count_request)['total']
+  end
+
+  def uri_to_json(url)
+    uri = URI(url)
+    request = Net::HTTP::get(uri)
+    movies_json = JSON.parse(request)
+  end
+end
