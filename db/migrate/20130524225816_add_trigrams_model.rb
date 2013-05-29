4012b9f3362d793bb413e113bd696a0d1d362a58
Sean Browning <seanbrowning@gmail.com>
2013-05-29 14:30:01 -0700

    redid trigrams

--- db/migrate/20130524225816_add_trigrams_model.rb
+++ db/migrate/20130524225816_add_trigrams_model.rb
@@ -1,3 +1,13 @@
 class AddTrigramsModel < ActiveRecord::Migration
-  extend Fuzzily::Migration
+  def up
+    create_table :trigrams do |t|
+      t.string :trigram, limit: 3
+      t.integer :score, limit: 2
+      t.integer :owner_id
+      t.integer :owner_type
+      t.string :fuzzy_field
+    end
+    add_index :trigrams, [:owner_id, :owner_type, :fuzzy_field, :trigram, :score], name: 'index_for_match'
+    add_index :trigrams, [:owner_id, :owner_type], name: 'index_by_owner'
+  end
 end
