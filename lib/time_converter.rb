0c8d150984dc672364d42c8469e574f4e03319f8
Ed Shadi <edshadi@gmail.com>
2013-05-29 10:40:30 -0700

    moved all date-time related helper methods to a TimeConverter module and cleaned up application_controller.

--- lib/time_converter.rb
+++ lib/time_converter.rb
@@ -1,22 +1,32 @@
-class DateTimeConverter
+module TimeConverter
 
   TIMES = [ "8:00a","9:00a","10:00a","11:00a","Noon", "1:00p",
-            "2:00p","3:00p","4:00p","5:00p", "6:00p", "7:00p", 
-            "8:00p", "9:00p", "10:00p", "11:00p", "12:00a" ]
+            "2:00p","3:00p","4:00p","5:00p", "6:00p", "7:00p",
+            "8:00p","9:00p","10:00p","11:00p","12:00a" ]
 
-  def self.time_range
-    values = (8..24).to_a
-    TIMES.zip(values)
+  def current_timezone_string
+    ActiveSupport::TimeZone.find_by_zipcode(current_zipcode)
   end
 
-  def self.day_range
-    name_days = (2..7).to_a.map {|n| n.days.from_now.strftime('%A')}
+  def day_range(time_zone)
+    name_days = (2..7).to_a.map.each do |n|
+      Time.use_zone(time_zone) { n.days.from_now.strftime('%A') }
+    end
+
     days = ["Today", "Tomorrow"]+ name_days
     days.zip((0..7).to_a)
   end
-
-  def self.get_datetime(day, time)
-    Chronic.parse("#{day} days from now at #{time}")
+  def datetime_in_utc(day, time, time_zone)
+    local = time_zone.at(day.to_i.days.from_now).change(:hour => time.to_i)
+    local.utc
   end
 
+  def display_time(time)
+    time.in_time_zone(current_timezone_string).strftime('%-l:%M%P')
+  end
+
+  def time_range
+    values = (8..24).to_a
+    TIMES.zip(values)
+  end
 end
