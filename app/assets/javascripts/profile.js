a487413b9931281bc53d741341d8f14e444d5f36
vincentstorme <storme.vincent@gmail.com>
2013-05-29 11:41:39 -0700

    a pie chart, but not the pie chart we need

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -7,7 +7,30 @@ function tabbing() {
   });
 };
 
+
+
 $(document).ready(function() {
   tabbing();
+
+  require([
+    "dojox/charting/Chart",
+    "dojox/charting/plot2d/Pie",
+    "dojox/charting/action2d/Tooltip",
+    "dojox/charting/action2d/MoveSlice",
+    "dojox/charting/themes/PlotKit/green",
+    "dojo/domReady!"
+], function(Chart, Pie, Tooltip, MoveSlice, PlotKitGreen){
+ 
+    var pieChart = new Chart("pieChartNode");
+    pieChart.setTheme(PlotKitGreen);
+    pieChart.addPlot("default", {
+        type: "Pie",
+        fontColor: "#000"
+    });
+    pieChart.addSeries("Series A", [5,8,10,40,20]);
+    new MoveSlice(pieChart,"default");
+    new Tooltip(pieChart,"default");
+    pieChart.render();
+});
 });
 
