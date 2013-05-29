0b5b621ccbab978ac1dbaef1a8de5a699f1c95ac
vincentstorme <storme.vincent@gmail.com>
2013-05-28 19:22:35 -0700

    refactor profile.js

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
+  tabbing();
+});
+
+function tabbing() {
+  $infoBox = $(this).siblings('.info-box');
+  $graphBox= $(this).siblings('.graph-box');
+
   $('.graphs').on('click', function(){
-    console.log($(this).siblings('.graph-box'))
-    $(this).siblings('.info-box').hide();
-    $(this).siblings('.graph-box').show();
+    $infoBox.hide();
+    $graphBox.show();
   });
 
   $('.info').on('click', function(){
-    $(this).siblings('.info-box').show();
-    $(this).siblings('.graph-box').hide();
+    $graphBox.hide();
+    $infoBox.show();
   });
-});
+};
