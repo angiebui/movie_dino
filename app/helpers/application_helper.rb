d568bea78572043e2302ffe9fa61c0d48db9bf9b
angiebui <angiebui@gmail.com>
2013-05-30 16:36:44 -0700

    no more unnecessary titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,7 @@ module ApplicationHelper
   end
 
   def default_poster
-    "http://www.google.com/imgres?um=1&sa=N&hl=en&biw=1212&bih=1009&tbm=isch&tbnid=4K3ZkQGE-2zC1M:&imgrefurl=http://www.tumblr.com/tagged/cute%2520dinosaurs&docid=rGcbLI-LevhDOM&imgurl=http://25.media.tumblr.com/tumblr_lffzt5IUZD1qgvv79o1_500.jpg&w=500&h=326&ei=_sinUeWBJMaQiQKD0ICQBA&zoom=1&iact=rc&dur=19&page=1&tbnh=181&tbnw=278&start=0&ndsp=29&ved=1t:429,r:0,s:0&tx=224&ty=107"
+    "https://s3-us-west-2.amazonaws.com/moviedino/default-poster.png"
   end
 
 end
